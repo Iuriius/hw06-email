686bf39ec57f79697a00a6cdd9039ae1ebbc312f
Iuriius <105295809+Iuriius@users.noreply.github.com>
2023-06-25 18:31:14 +0300

    1

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const contactsRouter = require("./routes/api/contacts");
 const authRouter = require("./routes/api/auth");
 
 const app = express();
-
+// const a = structuredClone(logger)
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
